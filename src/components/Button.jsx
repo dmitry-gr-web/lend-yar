4844fb8103bc8ee8426b47b1f39974da5f8070b8
Dima <dmitry.gr.web@gmail.com>
2025-04-18 16:14:39 +0300

    add link

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,16 @@ import listik from '../assets/Listik.svg';
 import '../styles/Button.scss';
 function Button() {
   return (
-    <button className={'btn'}>
+    <button
+      className={'btn'}
+      onClick={() => {
+        setTimeout(() => {
+          window.open(
+            'https://docs.google.com/forms/d/1ZlV9_QXA1v5A-IUQ1cWbVqCJy_yr92ZG7F90MRpkqHI/viewform?edit_requested=true',
+            '_blank',
+          );
+        }, 300);
+      }}>
       JOIN US!
       <img className="listik left" src={listik} />
       <img className="listik right" src={listik} />
