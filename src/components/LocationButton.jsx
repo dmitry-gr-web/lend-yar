3ee5008ee2886c18a20276dd52f03ac905abe0f7
Dima <dmitry.gr.web@gmail.com>
2025-04-19 15:52:23 +0300

    add href to loaction button , fix ultra wide

--- src/components/LocationButton.jsx
+++ src/components/LocationButton.jsx
@@ -3,7 +3,9 @@ import location from '../assets/tabler_location.svg';
 import '../styles/LocationButton.scss';
 const LocationButton = () => {
   return (
-    <a className="locationlink">
+    <a
+      className="locationlink"
+      href="https://www.google.com/maps/place/LS+Location/@48.3464071,34.9410697,14.99z/data=!4m6!3m5!1s0x40dbf1df1cb5d231:0xbfcdf82307517e03!8m2!3d48.3479167!4d34.9359231!16s%2Fg%2F11vbwld22b?hl=uk&entry=ttu&g_ep=EgoyMDI1MDQxNi4xIKXMDSoASAFQAw%3D%3D">
       <img src={location} />
       Direct Location
     </a>
